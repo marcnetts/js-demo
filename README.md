58d199d3a583e71f79c3b236342a21928480776e
marcnetts <marcnetts@gmail.com>
2022-08-23 21:09:54 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-c8tmdw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-c8tmdw)
---
+++ index.js
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>É nois que voa bruxao</h1>
+sdfsdf`;
