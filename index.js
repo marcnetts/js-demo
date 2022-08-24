a9554d15238677c09aed8f4b8ed9f779f452ed79
marcnetts <marcnetts@gmail.com>
2022-08-23 21:23:02 -0300

    2 files modified

--- index.js
+++ index.js
@@ -2,6 +2,21 @@
 import './style.css';
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>É nois que voa bruxao</h1>
-sdfsdf`;
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>É nois que voa bruxao</h1>;
+function getNumber(inputID) {
+  const value = document.getElementById(inputID).value;
+  return parseInt(value);
+}
+function getFunctionalFactorial(n) {
+  console.log(n);
+  if (n == 1) return 1;
+  else return (n = getFunctionalFactorial(n - 1));
+}
+function displaySum(value) {
+  document.getElementById('func-sum-result').textContent = value;
+}
+document.getElementById('func-btn').onclick = function handler() {
+  const res = getFunctionalFactorial(getNumber('input-n'));
+  displaySum(res);
+};
